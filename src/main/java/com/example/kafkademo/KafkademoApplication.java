c9c564a78e818a9a2b4560d156eab0cbd84de695
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-02 07:44:25 -0500

    Add message send fn

--- src/main/java/com/example/kafkademo/KafkademoApplication.java
+++ src/main/java/com/example/kafkademo/KafkademoApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkademoApplication {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        kafkaTemplate.send("tutorialspoint", msg);
+    }
     public static void main(String[] args) {
         SpringApplication.run(KafkademoApplication.class, args);
     }
