abc8a7c1b554d2c54c2607db8fc94dc8050725f6
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-02 07:28:15 -0500

    Add Imports

--- src/main/java/com/example/kafkademo/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafkademo/KafkaConsumerConfig.java
@@ -1,8 +1,12 @@
 package com.example.kafkademo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.ConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableKafka
 @Configuration
