4ba557c873f73232335af304c729101654eccd8d
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-02 07:22:51 -0500

    Add KafkaProducerConfig

--- src/main/java/com/example/kafkademo/KafkaProducerConfig.java
+++ src/main/java/com/example/kafkademo/KafkaProducerConfig.java
@@ -1,9 +1,19 @@
 package com.example.kafkademo;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
 
 }
