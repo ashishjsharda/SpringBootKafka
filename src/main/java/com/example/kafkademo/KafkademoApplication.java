1a020f2623bef50b21d5813a6f30e4740e1251dc
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-02 07:47:52 -0500

    Update imports ,cleanup

--- src/main/java/com/example/kafkademo/KafkademoApplication.java
+++ src/main/java/com/example/kafkademo/KafkademoApplication.java
@@ -1,8 +1,10 @@
 package com.example.kafkademo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
@@ -19,7 +21,7 @@ public class KafkademoApplication {
     public void listen(String message) {
         System.out.println("Received Messasge in group - group-id: " + message);
     }
-    @Override
+
     public void run(ApplicationArguments args) throws Exception {
         sendMessage("Hi Welcome to Kafka Example");
     }
